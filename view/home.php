aed7a927442c52a55e5539592093eaf8c4b02d7b
Lav <78282185+lavnisy@users.noreply.github.com>
2022-10-29 18:48:24 -0300

    Add files via upload

--- view/home.php
+++ view/home.php
@@ -38,6 +38,121 @@ $home = new HomeView();
         </nav>
     </section>
 
+    <main>
+        <section style="display:block;" id="Sobre" class="func-section">
+            <div class="about">
+                <div class="tittle-div">
+                    <img class="ant-img">
+                    <br>
+                    <h1 class="tittle">Antodo</h1>
+                    <br>
+                    <p class="text">
+                        No decorrer dos dias as pessoas acabam não
+                        gerenciando bem o seu tempo e acabam perdendo a
+                        oportunidade de fazerem uma semana,mês e ano mais
+                        produtivos.<br>
+                        Antodo vêm com o intuito de ajudar na questão da
+                        organização pessoal e acabar com problemas de falta
+                        de gestão do tempo.<br>
+                        Nossa aplicação vem com 3 funcionalidades que vão
+                        ajudar nossos usuários a gerenciar seu tempo e
+                        dinheiro:Cronograma Semanal Tarefas de Projetos e
+                        Gerenciador Financeiro.
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section style="display:none;" id="Cronograma Semanal" class="func-section">
+            <table id="cs-table" class="table">
+                <thead class="t-head">
+                    <tr class="t-row">
+                        <th class="camp">Segunda</th>
+                        <th class="camp">Terça</th>
+                        <th class="camp">Quarta</th>
+                        <th class="camp">Quinta</th>
+                        <th class="camp">Sexta</th>
+                        <th class="camp">Sábado</th>
+                    </tr>
+                </thead>
+                <tbody class="t-body">
+                    <tr class="t-row"></tr>
+                    <tr class="t-row"></tr>
+                    <tr class="t-row"></tr>
+                    <tr class="t-row"></tr>
+                    <tr class="t-row"></tr>
+                    <tr class="t-row"></tr>
+                    <tr class="t-row"></tr>
+                </tbody>
+            </table>
+        </section>
+
+        <section style="display:none;" id="Tarefas de Projetos" class="func-section">
+            <h1 class="tittle">Projetos</h1>
+            <div>
+                <div class="box-project">
+                    Criar Um Novo Projeto
+                </div>
+                <div class="box-project">
+                    Criar Um Novo Projeto
+                </div>
+                <div class="box-project">
+                    Criar Um Novo Projeto
+                </div>
+                <div class="box-project">
+                    Criar Um Novo Projeto
+                </div>
+            </div>
+        </section>
+
+        <section style="display:none;" id="Atividades-Projetos" class="func-section">
+            <table id="tp-table" class="table">
+                <thead class="t-head">
+                    <tr class="t-row">
+                        <th class="camp">Fazer</th>
+                        <th class="camp">Fazendo</th>
+                        <th class="camp">Feito</th>
+                    </tr>
+                </thead>
+                <tbody class="t-body">
+                    <tr class="t-row"></tr>
+                    <tr class="t-row"></tr>
+                    <tr class="t-row"></tr>
+                    <tr class="t-row"></tr>
+                    <tr class="t-row"></tr>
+                    <tr class="t-row"></tr>
+                    <tr class="t-row"></tr>
+                </tbody>
+            </table>
+        </section>
+
+        <section style="display:none;" id="Gerenciador Financeiro" class="func-section">
+            <div class="money">
+                <div class="box-money">
+                    <p>Contas</p>
+                    <p>-R$0,00</p>
+                </div>
+                <div class="box-money">
+                    <p>Ganhos</p>
+                    <p>+R$0,00</p>
+                </div>
+                <div class="box-money">
+                    <p>Saldo Final</p>
+                    <p>-R$0,00</p>
+                </div>
+            </div>
+            <table id="gf-table" class="table">
+                <thead class="t-head">
+                    <tr class="t-row">
+                        <th class="camp">Contas</th>
+                        <th class="camp">Ganhos</th>
+                    </tr>
+                </thead>
+            </table>
+        </section>
+        
+    </main>
+
     <script type="module" src="../javascript/main-home.js"></script>
 </body>
 </html>
