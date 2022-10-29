1f2b2edb4f389ea2984ce1f6d1e63166f3095294
Lav <78282185+lavnisy@users.noreply.github.com>
2022-10-29 18:43:14 -0300

    Add files via upload

--- javascript/main-home.js
+++ javascript/main-home.js
@@ -1,7 +1,47 @@
 import Home from '../lib/Home.class.js';
 
+let tables = {}
 let username = document.getElementById('username');
+let mainSections = document.getElementsByClassName('func-section');
+let navBarList = document.getElementsByClassName('list-item');
+let projects = document.getElementsByClassName('box-project');
+
+tables['Cronograma Semanal'] = {
+    'id':'cs-table',
+    'objectCamp':{
+        'Segunda': 0,
+        'Terça': 0,
+        'Quarta': 0,
+        'Quinta': 0,
+        'Sexta': 0        
+    },
+    'data':'tarefa',
+    'referenceCamp':'dia',
+    'referenceDatabase':'tarefa'
+};
+
+tables['Atvidades-Projetos'] = {
+    'id':'tp-table',
+    'objectCamp': {
+        'Fazer': 0,
+        'Fazendo': 0,
+        'Feito': 0 
+    },
+    'data':'nomeAtividade',
+    'referenceCamp':'estadoAtividade',
+    'referenceDatabase':'atividade'
+}
+
+
 let url = '../index.php';
 
-let home = new Home(username,url);
+let home = new Home(username,tables,url);
+
+home.chooseSection(mainSections,navBarList);
+
+projects[0].addEventListener('click',event => {
+    let projectTable = document.getElementById('Atividades-Projetos');
+    projectTable.style.display = 'block';
+    projects[0].style.display = 'none';
+})
 
