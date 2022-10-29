d853dee5c22c1037713840dd488ddbefd7cba159
Lav <78282185+lavnisy@users.noreply.github.com>
2022-10-29 18:44:49 -0300

    Add files via upload

--- model/query.php
+++ model/query.php
@@ -6,11 +6,23 @@ include '../lib/Model.class.php';
 session_start();
 
 if(isset($_SESSION['dataRequire'])){
+
     unset($_SESSION['dataRequire']);
-    
+
     $model = new Model($connect,$_SESSION['email'],$_SESSION['senha']);
 
-    $_SESSION['backData'] = $model->getUsername();
+    $data = $model->getAllUserTablesData($tables['user']);
+
+    foreach($data['projeto'] as $projeto){
+        $data['atividade'] = $model->selectWhere(
+            'atividade',
+            '*',
+            'idProjeto',
+            $projeto['idProjeto']);
+    }
+    
+    $_SESSION['backData'] = $data;
+    
     header("location:../index.php");
 }
 
